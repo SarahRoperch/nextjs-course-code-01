35c9ffdf4de1f3cce230929bc0e22c9810d1b326
Sarah Roperch <sarah.roperch@nexelec.fr>
2024-03-20 16:58:13 +0100

    first commit

---
+++ pages/clients/index.js
@@ -0,0 +1,21 @@
+import Link from "next/link";
+
+function ClientsPage() {
+  const clients = [
+    { id: "max", name: "Maximillian" },
+    { id: "manu", name: "Manuel" },
+  ];
+  return (
+    <div>
+      <h1>The Client Page</h1>
+      <ul>
+        {clients.map((client) => (
+          <li key={client.id}>
+            <Link href={`/clients/${client.id}`}>{client.name}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+export default ClientsPage;
